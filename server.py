b88d1997c79397e9a0328fe25ea8558233a7ef16
Bhupinder121 <Bhupinder.palsingh121@gmail.com>
2024-03-10 14:55:10 +0530

    streaming sorted!!!

--- server.py
+++ server.py
@@ -1,5 +1,25 @@
 import numpy as np
 import cv2
 from PIL import ImageGrab
+import socket
+import pickle
+import struct
 
-img = ImageGrab.grab(bbox=(3200, 360, 3350,400), all_screens=True) #x, y, w, h
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(('0.0.0.0', 8888))
+server_socket.listen(5)
+print("Server is listening...")
+client_socket, client_address = server_socket.accept()
+print(f"Connection from {client_address} accepted")
+while True:
+    frame = np.array(ImageGrab.grab(bbox=(0, 0, 400, 400), all_screens=True))
+
+    frame_data = pickle.dumps(frame)
+    client_socket.sendall(struct.pack("Q", len(frame_data)))
+    client_socket.sendall(frame_data)
+    cv2.imshow('Server', frame)
+    if cv2.waitKey(1) == 13:
+        break
+cap.release()
+cv2.destroyAllWindows()
